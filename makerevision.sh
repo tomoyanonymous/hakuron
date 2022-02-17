d38d5ab404d42f67ba3a8c37ffaedde61ca0e486
Tomoya Matsuura(MacBookPro) <me@matsuuratomoya.com>
2022-02-17 07:16:26 +0000

    time format for revision script

--- makerevision.sh
+++ makerevision.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-filename=$(echo revisions/MatsuuraHakuron$(date "+%Y%m%d").pdf)
+filename=$(echo revisions/MatsuuraHakuron$(TZ=JST-9 date "+%Y%m%d").pdf)
 
 cp src/template.pdf $filename
