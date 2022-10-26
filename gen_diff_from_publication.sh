ecf0acd7139550b1be306bd7fab7971c6c26c427
Tomoya Matsuura(MacBookPro) <me@matsuuratomoya.com>
2022-10-27 00:26:35 +0900

    diff 思ったより読みづらいので直した

--- gen_diff_from_publication.sh
+++ gen_diff_from_publication.sh
@@ -2,10 +2,11 @@
 pub_hash=e4372e639ca94ba91ec11e6120687745a7b5660d
 mkdir tmp
 dest="# 出版時からの正誤表（Git履歴より自動生成）\n\n"
-for md in $(find *.md)
+for md in $(find chapter*.md -not -name '_index.md' -not -name 'readme.md')
 do 
 echo "processing $md"
-diff=$(git diff --word-diff HEAD $pub_hash $md)
+# diff=$(git diff --word-diff HEAD $pub_hash $md) #思ったより読みづらい
+diff=$(git diff HEAD $pub_hash $md) 
 if [ -n "$diff" ];#contain some diff
 then
 echo "$md contains diff"
@@ -14,3 +15,4 @@ fi
 done
 echo $dest >| tmp/revisions.md
 pandoc tmp/revisions.md  --metadata title="松浦知也博士論文 正誤表" --highlight-style=espresso  --template=chapter_template.html.template -o  revisions_rendered.html
+rm -rf .tmp
