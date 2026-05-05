017649663949307368686d3d18b5ab50fa6f4bf2
Tomoya Matsuura(MBA2025) <me@matsuuratomoya.com>
2026-05-05 13:55:21 +0900

    fixed html rendering

--- gen_html.sh
+++ gen_html.sh
@@ -10,7 +10,7 @@ offset=$(echo "$md" | sed  -E 's/chapter([0-9])\.md/\1/')
 echo ${offset}
 fi
 echo "heading offset is $offset"
-pandoc $md --number-offset=$(($offset)) --highlight-style=espresso --shift-heading-level-by=1 --filter pandoc-crossref -M "crossrefYaml=./pandoc-crossref-config.yml" --citeproc  --bibliography=ref.bib --template=chapter_template.html.template -o  ${md%.*}_rendered.html
+pandoc $md --number-offset=$(($offset)) --syntax-highlighting=espresso --shift-heading-level-by=1 --filter pandoc-crossref -M "crossrefYaml=./pandoc-crossref-config.yml" --citeproc  --bibliography=ref.bib --template=chapter_template.html.template -o  ${md%.*}_rendered.html
 done
 
 
